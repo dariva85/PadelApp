436ee625bdef2f1930556827c57a865c645b5231
David R <David.RigalVall@gmail.com>
2022-08-21 19:57:24 +0200

    Frontend first upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.css";
+import TopBar from "./components/TopBar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from "react";
+import LoginScreen from "./screens/LoginScreen";
+import MainScreen from "./screens/MainScreen";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <React.StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginScreen />}></Route>
+          <Route path="/main" element={<MainScreen />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </React.StrictMode>
+  );
 }
 
-export default App
+export default App;
