505488642981a6aa9c8ee5fa9bbd7b3187c4bb7d
Martí Ollé <martiollea@gmail.com>
2022-09-14 18:43:09 +0200

    Partidos de una competición

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -50,6 +50,11 @@ export const signUpOnCompetition = ({ signupdata }) =>
     signupdata
   );
 export const getMatches = (id) => authApiCall("GET", `/partidos/userId/${id}`);
+export const getMatchesOfOneCompetition = (ids) =>
+  authApiCall(
+    "GET",
+    `/competiciones/userId/${ids.userId}/${ids.competitionId}/Matches`
+  );
 export const getRanking = (id) =>
   authApiCall("GET", `/rankings/competition/${id}`);
 export const subscribeOrUnsubscribeOnCompetition = (ids) =>
@@ -58,6 +63,5 @@ export const subscribeOrUnsubscribeOnCompetition = (ids) =>
     `/competiciones/subscribeorunsubscrive/${ids.userId}/${ids.competitionId}`
   );
 export const getNamesUser = (id) => authApiCall("GET", `/usuarios/name/${id}`);
-
 export const submitMatch = (id, match) =>
   authApiCall("PUT", `/partidos/updatePartido/userId/${id}`, { match });
---
+++ frontend/src/screens/Competition/CompetitionMatchesScreenWIP.jsx
@@ -0,0 +1,46 @@
+import React, { Component, useEffect, useState, useContext } from "react";
+import { useParams } from "react-router-dom";
+import * as topBarCtxt from "../../components/TopBarCtxt";
+import MatchesCard from "../../components/MatchesCard";
+import "./CompetitionMatchesScreen.css";
+import * as api from "../../api/api.js";
+
+export default function CompetitionMatchesScreen() {
+  const { competitionId } = useParams();
+  const [competition, setCompetition] = useState([]);
+  const [names, setNames] = useState([]);
+  const [matches, setMatches] = useState([]);
+  const { topBarInfo, setTopBarInfo } = useContext(topBarCtxt.Ctxt);
+
+  const LoadMatches = async () => {
+    const {
+      success,
+      result: Matches,
+      error,
+    } = await api.getMatchesOfOneCompetition(competitionId);
+    if (success) {
+      setMatches(Matches.results[0]);
+    } else {
+      setMessage(error);
+    }
+  };
+
+  useEffect(() => {
+    LoadMatches();
+    topBarCtxt.setTopBarInfo(
+      topBarCtxt.menuByCompetition(competitionId).MatchesScreen,
+      topBarInfo,
+      setTopBarInfo
+    );
+  }, []);
+
+  const AddMatchesCards = (competitionItems, names) => {
+    return competitionItems.map((item) => <MatchesCard matches={item} S />);
+  };
+
+  return (
+    <div className="main-screen">
+      <div className="matches-container">{AddMatchesCards(matches, names)}</div>
+    </div>
+  );
+}
