f376120efc87754960a389d92ac5f4c6141c9ac5
Alex López <lopezpedrosa@gmail.com>
2022-09-15 12:45:06 +0200

    Adding features to PasswordChangeScreen

--- frontend/src/screens/Usuario/PasswordChange.jsx
+++ frontend/src/screens/Usuario/PasswordChange.jsx
@@ -1,10 +1,57 @@
 import React, { useState, useEffect, useContext } from "react";
+import { BsFillEyeFill, BsFillEyeSlashFill} from "react-icons/bs";
 import "./PasswordChange.css";
 import * as topBarCtxt from "../../components/TopBarCtxt";
 
 export default function PasswordChange() {
   const { topBarInfo, setTopBarInfo } = useContext(topBarCtxt.Ctxt);
   const [newPassword, setNewPassword] = useState("");
+  const [currentPasswordType, setCurrentPasswordType] = useState("password");
+  const [newPasswordType, setNewPasswordType] = useState("password");
+  const [repeatPasswordType, setRepeatPasswordType] = useState("password");
+  const [currentPassword, setCurrentPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
+
+  const togglePassword = ()=>{
+    if(passwordType==="password")
+    {
+     setPasswordType("text");
+     return;
+    }
+    setPasswordType("password");
+  }
+
+  const startVisibilityIcon = (passwordType, passwordId) => {
+    if(passwordType == 'text') {
+      return (
+        <button className="icon" onClick={() => {
+          if(passwordId === 'new') {
+            setNewPasswordType('password');
+          } else if(passwordId === 'current') {
+            setCurrentPasswordType('password');
+          } else if(passwordId === 'repeat') {
+            setRepeatPasswordType('password');
+          }
+        }
+          }>
+          <BsFillEyeFill />
+        </button> );
+    } else {
+        return (
+            <button className="icon" onClick={() => {
+              if(passwordId === 'new') {
+                setNewPasswordType('text');
+              } else if(passwordId === 'current') {
+                setCurrentPasswordType('text');
+              } else if(passwordId === 'repeat') {
+                setRepeatPasswordType('text');
+              }
+            }
+              }>
+              <BsFillEyeSlashFill />
+            </button> );
+    }
+  }
 
   useEffect(() => {
     topBarCtxt.setTopBarInfo(
@@ -18,24 +65,42 @@ export default function PasswordChange() {
     event.preventDefault();
   }
 
+  const reset = () => {
+    setCurrentPassword("");
+    setNewPassword("");
+    setRepeatPassword("");
+    setNewPasswordType("password");
+    setCurrentPasswordType("password");
+    setRepeatPasswordType("password");
+  }
+
   return (
     <div id="passwordChange-screen">
       <form id="password-form" onSubmit={handleSubmit}>
         <label className ="label-form">
           Contraseña actual
-          <input className="box-form" type="text" name="Contraseña actual" />
+          <div class="show-password">
+            <input className="box-form" type={currentPasswordType} onChange={e => setCurrentPassword(e.target.value)} value={currentPassword} name="Contraseña actual" />
+            {startVisibilityIcon(currentPasswordType, 'current')}
+        </div>
         </label>
         <label className ="label-form">
           Contraseña nueva
-          <input className="box-form" type="text" value={newPassword} onChange={(e) => setName(e.target.value)} name="Contraseña nueva" />
+          <div class="show-password">
+            <input className="box-form" type={newPasswordType} onChange={e => setNewPassword(e.target.value)} value={newPassword} name="Contraseña nueva" />
+            {startVisibilityIcon(newPasswordType, 'new')}
+        </div>
         </label>
         <label className ="label-form">
           Repite contraseña
-          <input className="box-form" type="text" name="Repite contraseña" />
+          <div class="show-password">
+            <input className="box-form" type={repeatPasswordType} onChange={e => setRepeatPassword(e.target.value)} value={repeatPassword} name="Repite contraseña" />
+            {startVisibilityIcon(repeatPasswordType, 'repeat')}
+        </div>
         </label>
         <div id="buttons-form">
-          <button style={{color:"green"}} className="submit-button">Aceptar</button>
-          <button style={{color:"red"}} className="submit-button">Cancelar</button>
+          <button style={{color:"green"}} onClick={handleSubmit} className="submit-button">Aceptar</button>
+          <button style={{color:"red"}} onClick={reset} className="submit-button">Cancelar</button>
         </div>
       </form>
     </div>
